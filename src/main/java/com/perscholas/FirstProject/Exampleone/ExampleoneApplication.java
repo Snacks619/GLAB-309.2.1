94922b532f49cc32ab2697363b91153206177da0
Snacks619 <167789557+Snacks619@users.noreply.github.com>
2024-08-14 09:23:43 -0400

    Finished Assaignment

---
+++ src/main/java/com/perscholas/FirstProject/Exampleone/ExampleoneApplication.java
@@ -0,0 +1,13 @@
+package com.perscholas.FirstProject.Exampleone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/perscholas/FirstProject/Exampleone/ExampleoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perscholas.FirstProject.Exampleone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
